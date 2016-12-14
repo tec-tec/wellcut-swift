6ac4c2f895fa05a0fbc2253314606290f059e5d9
Ludovic Ollagnier <ludovic@tectec.fr>
2016-12-14 10:01:44 +0100

    Initial Commit

---
+++ WhereToEat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhereToEat
+//
+//  Created by Ludovic Ollagnier on 14/12/2016.
+//  Copyright © 2016 Ludovic Ollagnier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
