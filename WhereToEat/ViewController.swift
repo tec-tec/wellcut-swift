bbb0d02952a8437826a32ebd1ded69f3d4db420c
Ludovic Ollagnier <ludovic@tectec.fr>
2016-12-14 16:19:32 +0100

    Form with error management

--- WhereToEat/ViewController.swift
+++ WhereToEat/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum FormError: Error {
+        case noText(textField: UITextField)
+        case notEnoughtCharacters (textField: UITextField, minNumbersOfChar: Int)
+    }
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var styleTextField: UITextField!
+    
+    var directory = Directory()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +31,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    private func restaurantFromForm() -> Restaurant? {
+    private func restaurantFromForm() throws -> Restaurant {
         
-        return nil
+        guard let name = nameTextField.text else {
+            throw FormError.noText(textField: nameTextField)
+        }
+        guard name.characters.count >= 2 else {
+            throw FormError.notEnoughtCharacters(textField: nameTextField, minNumbersOfChar: 2)
+        }
+        guard let address = addressTextField.text else {
+            throw FormError.noText(textField: addressTextField)
+        }
+        guard address.characters.count >= 2 else {
+            throw FormError.notEnoughtCharacters(textField: addressTextField, minNumbersOfChar: 2)
+        }
+        
+        let resto = Restaurant(name: name, address: address)
+        return resto
     }
 
     @IBAction func cancel(_ sender: Any) {
     }
     
     @IBAction func save(_ sender: Any) {
+        
+        nameTextField.backgroundColor = UIColor.white
+        addressTextField.backgroundColor = UIColor.white
+        
+        do {
+            let resto = try restaurantFromForm()
+            directory.add(resto)
+        } catch FormError.noText(let textField) {
+            textField.backgroundColor = UIColor.red
+        } catch FormError.notEnoughtCharacters (let textField, let minNumbersOfChar){
+            textField.backgroundColor = UIColor.red
+            print("\(minNumbersOfChar)")
+        } catch {
+            
+        }
     }
 }
 
