b90a5b2751e04dbef9ec902d0a4e9caa135266c8
Ludovic Ollagnier <ludovic@tectec.fr>
2017-01-19 17:34:33 +0100

    Background execution example

--- WhereToEat/AppDelegate.swift
+++ WhereToEat/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CloudKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,28 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        if !UserDefaults.standard.bool(forKey: "sub") {
-            let pred = NSPredicate(value: true)
-            let sub = CKSubscription(recordType: "Restaurant", predicate: pred, options: .firesOnRecordCreation)
-            
-            let notInfo = CKNotificationInfo()
-            notInfo.alertBody = "Nouveau resto !"
-            
-            sub.notificationInfo = notInfo
-            
-            let db = CKContainer.default().publicCloudDatabase
-            db.save(sub, completionHandler: { (sub, error) in
-                if let e = error {
-                    print(e.localizedDescription)
-                } else {
-                    UserDefaults.standard.set(true, forKey: "sub")
-                }
-            })
-        }
-        application.registerForRemoteNotifications()
-        application.registerUserNotificationSettings(UIUserNotificationSettings(types: .alert, categories: nil))
- 
+
         return true
     }
 
@@ -47,13 +25,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
+    var backgroundTaskID: UIBackgroundTaskIdentifier!
+    var timer: Timer?
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+//        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.ping), userInfo: nil, repeats: true)
+
+        backgroundTaskID = application.beginBackgroundTask(withName: "Task") {
+            // Clean up any unfinished task business by marking where you
+            // stopped or ending the task outright.
+            
+            print("Times up!")
+            application.endBackgroundTask(self.backgroundTaskID)
+            self.backgroundTaskID = UIBackgroundTaskInvalid
+        }
+        
+        // Start the long-running task and return immediately.
+//        DispatchQueue.global(qos: .background).async {
+            // Do the work associated with the task, preferably in chunks.
+            
+            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.ping), userInfo: nil, repeats: true)
+            //When finished, end background task and invalidate the ID
+            application.endBackgroundTask(self.backgroundTaskID)
+            self.backgroundTaskID = UIBackgroundTaskInvalid
+//        }
+
+    }
+    
+    func ping() {
+        print("Ping \(Date())")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        timer?.invalidate()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -62,6 +69,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        
     }
 
 
